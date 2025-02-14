bb013a46ec35344533f3d97a88164bd41c82e67e
Daappoy <m.alghifaridaffa@gmail.com>
2025-02-14 16:26:43 +0700

    Main Menu added
    Main Menu remains in progress, empty objects for options and level menu's have been added

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using TMPro;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void OpenSettings()
+    {
+
+    }
+    
+    public void OpenLevels()
+    {
+
+    }
+    
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
