ffad8686c3c90a49d4a642cf7ac88c37c7f0ccaa
Daappoy <m.alghifaridaffa@gmail.com>
2025-03-18 16:36:45 +0700

    script crow movement updated
    hanya ada tambahan movement di script crow

--- Assets/Scripts/Player/CrowScript.cs
+++ Assets/Scripts/Player/CrowScript.cs
@@ -13,7 +13,12 @@ public class CrowScript : MonoBehaviour
 
     [Header("crow Movement")]
     [SerializeField] private float speed = 5f;
-    [SerializeField] public float flyStrength = 5f;
+    [SerializeField] public float flyStrength = 5f; 
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck; 
+    [SerializeField] private float checkRadius = 0.9f;
+    [SerializeField] private LayerMask groundLayer;
 
     // Start is called before the first frame update
     void Start()
@@ -23,15 +28,37 @@ public class CrowScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        
+       float horizontalInput = Input.GetAxis("Horizontal");
+       rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);  
+
+
+        // Flip the sprite based on the direction the player is moving (VISUAL)
+       if (horizontalInput > 0)
+        {
+            transform.localScale = new Vector3(0.5f, 0.5f, 0.5f); // Facing right
+        }
+        else if (horizontalInput < 0)
+        {
+            transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f); // Facing left
+        }
     }
     
     // Update is called once per frame
     void Update()
     {
-         if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            
+            isFlying = true;
+            rb.velocity = Vector2.up * flyStrength;
+        }
+        else if(Input.GetKey(KeyCode.Space))
+        {
+            // rb.gravityScale = 0.1f;
+        }
+        else 
+        {
+            rb.gravityScale = 1f;
+            isFlying = false;
         }
     }
 }
