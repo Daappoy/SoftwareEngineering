5f6f4c9d5d6935167bd5863086091d64567dfb12
RandomHunter33 <loubeneden@gmail.com>
2025-06-10 21:08:10 +0700

    try using quaternion

--- Assets/Scripts/RotatorScript.cs
+++ Assets/Scripts/RotatorScript.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class RotatorScript : MonoBehaviour
 {
+    public PlayerSwitch PS;
     public List<GameObject> RotatorList;
     public float rotationSpeed = 100f;
+    public Camera _camera;
 
-    void Update()
+    //void Update()
+    //{
+    //    transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+    //}
+
+    public void turnControlOff ()
     {
-        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        PS.FoxController.InputEnabled = false;
+        PS.CrowController.CrowInputEnabled = false;
+        PS.enabled = false;
+        StartCoroutine(turnControlOn());
+    }
+
+    public IEnumerator turnControlOn ()
+    {
+        yield return new WaitForSecondsRealtime(2.0f);
+        PS.enabled = true;
+        if (PS.isFox)
+        {
+            PS.FoxController.InputEnabled = true;
+        }
+        else
+        {
+            PS.CrowController.CrowInputEnabled = true;
+        }
+    }
+
+    public void GameRotateLeft ()
+    {
+        turnControlOff();
+        _camera.transform.rotation = Quaternion.Euler(0, 0, 90);
     }
 }
